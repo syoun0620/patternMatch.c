86d865e6901e8acbf77207519948544c9c624bf6
sukwhan (david) <younsa@bc.edu>
2017-02-26 00:25:26 -0500

    Update patternMatch.c

--- patternMatch.c
+++ patternMatch.c
@@ -1,1 +1,78 @@
-// Sukwhan Youn// Computer Systems HW4// Due 2/24/2017#include <stdio.h>int readline(int *p1, int aNumber);int* findmatch(int *p1, int *p2, int number1, int number2);void printmessage(int *position, int *text, int patternlen, int textlen );int patternStart = 0;int indexT=0;		// index for Textint indexp=0;		// index for patternint main() {   int text[40], pattern[40], *position;   int textlen, patternlen;   printf("Enter text: ");   textlen = readline(text, 40);   printf("Enter pattern: ");   patternlen = readline(pattern, 40);   /*for (int i=0;i<textlen;i++){   	printf("%c ", text[i]);   }   for (int i=0;i<patternlen;i++){   	printf("%c ", pattern[i]);   }*/   //printf("textlen: %d ", textlen);   // printf("patternlen: %d ", patternlen);   position = findmatch(pattern, text, patternlen, textlen);   // printf("position: %p ", position);   printmessage(position, text, patternlen, textlen);}int readline(int *p1, int aNumber){	int c=getchar();	int i=0;	while (c != '\n' && c!='\0' && c!=32 && i<aNumber ){		*p1 = c;		p1++;		i++;		c=getchar();	}	return i;}int* findmatch(int *pattern, int *text, int patternlen, int textlen){ 	while (indexp<patternlen){		if (indexT==textlen){			return NULL;			break;		}		else if (*(pattern+indexp)=='?' || *(pattern+indexp) == *(text+indexT)){			indexp++;			indexT++;		}		else{			indexp=0;			patternStart++;			indexT=patternStart;		}	}	return (text + patternStart);}void printmessage(int *position, int *text, int patternlen, int textlen ){	if (indexp==patternlen){		printf("The pattern was found at char %d. The remaining text chars are: " ,patternStart+1 );		int z = patternlen;		for (z; z < textlen-patternStart; z++){			printf("%c",*(position+z));		}		printf("\n");		//printf("index:%d indexT:%d patternStart: %d, patternlen: %d, textlen:%d ", indexp, indexT,patternStart,patternlen,textlen);	}	else if (position==NULL){		printf("no match\n");		//printf("index:%d indexT:%d patternStart: %d, patternlen: %d, textlen:%d ", indexp, indexT,patternStart,patternlen,textlen);	}	//just for safety.	else{		printf("NO MATCH");	}}
+// Sukwhan Youn
+
+
+#include <stdio.h>
+int readline(int *p1, int aNumber);
+int* findmatch(int *p1, int *p2, int number1, int number2);
+void printmessage(int *position, int *text, int patternlen, int textlen );
+
+int patternStart = 0;
+int indexT=0;		// index for Text
+int indexp=0;		// index for pattern
+
+int main() {
+   int text[40], pattern[40], *position;
+   int textlen, patternlen;
+
+
+   printf("Enter text: ");
+   textlen = readline(text, 40);
+   printf("Enter pattern: ");
+   patternlen = readline(pattern, 40);
+   position = findmatch(pattern, text, patternlen, textlen);
+   printmessage(position, text, patternlen, textlen);
+}
+
+int readline(int *p1, int aNumber){
+	int c=getchar();
+	int i=0;
+	while (c != '\n' && c!='\0' && c!=32 && i<aNumber ){
+		*p1 = c;
+		p1++;
+		i++;
+		c=getchar();
+	}
+	return i;
+}
+
+int* findmatch(int *pattern, int *text, int patternlen, int textlen){ 
+	while (indexp<patternlen){
+		if (indexT==textlen){
+			return NULL;
+			break;
+		}
+		else if (*(pattern+indexp)=='?' || *(pattern+indexp) == *(text+indexT)){
+			indexp++;
+			indexT++;
+		}
+		else{
+			indexp=0;
+			patternStart++;
+			indexT=patternStart;
+		}
+	}
+	return (text + patternStart);
+}
+void printmessage(int *position, int *text, int patternlen, int textlen ){
+	if (indexp==patternlen){
+		printf("The pattern was found at char %d. The remaining text chars are: " ,patternStart+1 );
+		int z = patternlen;
+		for (z; z < textlen-patternStart; z++){
+			printf("%c",*(position+z));
+
+		}
+		printf("\n");
+	}
+	else if (position==NULL){
+		printf("no match\n");
+  }
+	else{
+		printf("NO MATCH");
+	}
+}
+
+
+
+
+
+
